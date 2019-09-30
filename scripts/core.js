76206fab13336abf6a37c74e88e79ad88baa9d59
iamcev <mailofrealcev@gmail.com>
2019-09-30 10:29:13 -0400

    starting classes

--- scripts/core.js
+++ scripts/core.js
@@ -1,3 +1,71 @@
-(function(global){
-    
+(function (global) {
+    class Player {
+        constructor(_name, _factory) {
+            this.name = _name;
+            this.factory = _factory;
+        }
+        money = 0;
+        name = 'Guest';
+        factory;
+        candy;
+    }
+    class Factory {
+        importDepot;
+        productionBay;
+        storageBuilding;
+        shippingDepot;
+    }
+    class ImportDepot {
+        workerSpeed = 1;
+        vehicleSpeed = 1;
+        vehicleCapacity = 1;
+        vehicleMaxCount = 0;
+
+        vehicles = [];
+
+        static Defaults = {
+            workerSpeed: 1,
+            vehicleSpeed: 5,
+            vehicleCapacity: 25,
+            vehicleMaxCount: 5
+        };
+    }
+    class ProductionBay {
+        workerSpeed = 1;
+        productionRate = 1;
+
+        static Defaults = {
+            
+        };
+
+        static Tiers = {
+
+        };
+    }
+    class StorageBuilding {
+        storageCapacity = 1;
+
+        static Defaults = {
+            
+        };
+
+        static Tiers = {
+
+        };
+    }
+    class ShippingDepot {
+        workerSpeed = 1;
+        vehicleSpeed = 1;
+        vehicleCapacity = 1;
+
+        static Defaults = {
+            
+        };
+
+        static Tiers = {
+
+        };
+    }
+
+    console.log(new Player('d'));
 })(this);
