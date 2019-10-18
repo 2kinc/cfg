3b380fd416fd9cf98376fe1944dc736185ba700a
iamcev <mailofrealcev@gmail.com>
2019-10-18 12:58:07 -0400

    fruck

--- scripts/data.js
+++ scripts/data.js
@@ -2,7 +2,7 @@ import { FileReader } from '../scripts/fileReader.js';
 
 var Data = {
     get: function (callback) {
-        FileReader.readTextFile('../scripts/upgrades.json', function (text) {
+        FileReader.readTextFile('./scripts/upgrades.json', function (text) {
             var data = JSON.parse(text);
             Data.Upgrades = data;
         });
